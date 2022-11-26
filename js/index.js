b47d2c7ad30141e3de9da674e0876c1b7a0e4b3b
李朝稼 <2017783133@qq.com>
2022-11-26 12:54:36 +0800

    更新代码

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="123" &&  pwd=="123"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			console.log(userName);
+			localStorage.setItem('name',userName)
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("Wrong Password");
+	}
+});
